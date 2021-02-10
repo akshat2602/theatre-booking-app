d25038c85dd5ab16aee4d02789dc9bc7a4525543
akshat2602 <akshatsharma2602@gmail.com>
2021-02-10 21:12:56 +0530

    Fixed linting errors

--- app/book/serializers.py
+++ app/book/serializers.py
@@ -2,15 +2,33 @@ from rest_framework import serializers
 
 
 class SeatAllotSerializer(serializers.Serializer):
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
     ticket = serializers.UUIDField()
     name = serializers.CharField(max_length=50)
 
 
 class SeatAllotResponseSerializer(serializers.Serializer):
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
     ticket = serializers.UUIDField()
     name = serializers.CharField(max_length=50)
     seat_number = serializers.IntegerField()
 
 
 class VacateSeatSerializer(serializers.Serializer):
+    def create(self, validated_data):
+        pass
+
+    def update(self, instance, validated_data):
+        pass
+
     seat_number = serializers.IntegerField()
--- app/book/views.py
+++ app/book/views.py
@@ -1,7 +1,9 @@
 from django.conf import settings
 from rest_framework import generics, status
 from rest_framework.response import Response
-from .serializers import SeatAllotSerializer, SeatAllotResponseSerializer, VacateSeatSerializer
+from .serializers import SeatAllotSerializer, \
+    SeatAllotResponseSerializer, \
+    VacateSeatSerializer
 
 seats = dict.fromkeys(i for i in range(1, settings.MAX_OCCUPANCY + 1))
 
@@ -21,7 +23,9 @@ class occupySeat(generics.CreateAPIView):
             for key, values in seats.items():
                 if seats[key] is not None:
                     if seats[key]['ticket'] == ticket:
-                        return Response(data={"Message": "A ticket with this UUID has already been booked!"})
+                        return Response(data={
+                            "Message": "A ticket with this UUID has already been booked!"
+                        })
                     else:
                         pass
                 else:
@@ -35,13 +39,18 @@ class occupySeat(generics.CreateAPIView):
                 "seat_number": seat_number,
             }
             print(seats)
-            response_serializer = SeatAllotResponseSerializer(data=data)
+            response_serializer = SeatAllotResponseSerializer(
+                data=data
+            )
             if response_serializer.is_valid():
-                return Response(response_serializer.data, status=status.HTTP_201_CREATED)
+                return Response(response_serializer.data,
+                                status=status.HTTP_201_CREATED)
 
-            return Response(response_serializer.errors, status=status.HTTP_404_NOT_FOUND)
+            return Response(response_serializer.errors,
+                            status=status.HTTP_404_NOT_FOUND)
 
-        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+        return Response(serializer.errors,
+                        status=status.HTTP_404_NOT_FOUND)
 
 
 class vacateSeat(generics.DestroyAPIView):
@@ -53,9 +62,15 @@ class vacateSeat(generics.DestroyAPIView):
             if seats[seat_number]:
                 seats[seat_number] = None
                 print(seats)
-                return Response(data={"Message": "The provided seat number has been vacated!"}, status=status.HTTP_204_NO_CONTENT)
+                return Response(data={
+                    "Message": "The provided seat number has been vacated!"
+                }, status=status.HTTP_204_NO_CONTENT)
             else:
                 print(seats)
-                return Response(data={"Message": "The provided seat number is vacant already!"}, status=status.HTTP_404_NOT_FOUND)
+                return Response(data={
+                    "Message": "The provided seat number is vacant already!"
+                }, status=status.HTTP_404_NOT_FOUND)
         print(seats)
-        return Response(data={"Message": "Seat number doesn't exist!"}, status=status.HTTP_404_NOT_FOUND)
+        return Response(data={
+            "Message": "Seat number doesn't exist!"
+        }, status=status.HTTP_404_NOT_FOUND)
